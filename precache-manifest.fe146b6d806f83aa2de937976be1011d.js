f7851130eb70fcd4e3ae36622e60fe657b524e4b
Hasan Saleem <55816213+hasan-umanitoba@users.noreply.github.com>
2020-06-11 06:33:03 -0500

    Updates

---
+++ precache-manifest.fe146b6d806f83aa2de937976be1011d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cc49f24dd8671a17faa555602298e8",
+    "url": "/personalPortfolio-Website/index.html"
+  },
+  {
+    "revision": "3b6e7dd1914bfa30da1e",
+    "url": "/personalPortfolio-Website/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "886536bc64188ccdb264",
+    "url": "/personalPortfolio-Website/static/css/main.eab60601.chunk.css"
+  },
+  {
+    "revision": "3b6e7dd1914bfa30da1e",
+    "url": "/personalPortfolio-Website/static/js/2.fb9a1508.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personalPortfolio-Website/static/js/2.fb9a1508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886536bc64188ccdb264",
+    "url": "/personalPortfolio-Website/static/js/main.9b023124.chunk.js"
+  },
+  {
+    "revision": "e4b173137af1f1afb825",
+    "url": "/personalPortfolio-Website/static/js/runtime-main.8d9b88b6.js"
+  },
+  {
+    "revision": "900abed3b9dcbe496b568d0296b8ce7c",
+    "url": "/personalPortfolio-Website/static/media/githubimage.900abed3.jpg"
+  },
+  {
+    "revision": "1b2ee1376b5afdf88025613680f84f30",
+    "url": "/personalPortfolio-Website/static/media/insta.1b2ee137.jpg"
+  },
+  {
+    "revision": "0c9ada9b5218f7f4e8e06219e3f4eac9",
+    "url": "/personalPortfolio-Website/static/media/lightblue.0c9ada9b.jpg"
+  },
+  {
+    "revision": "fa0fc338ad5bd530eaf9a9ebd31754f4",
+    "url": "/personalPortfolio-Website/static/media/linkedin.fa0fc338.jpg"
+  }
+]);
